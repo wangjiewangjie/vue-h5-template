ed3deba33ff9aae36a9708932429b745dfbae112
wangjie <wangjie0107@aliyun.com>
2020-05-26 17:59:20 +0800

    按需引入vant组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="page">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+
     <Tabbar />
   </div>
 </template>
 
 <script>
-// import { Swipe, SwipeItem } from "vant";
+import { Swipe, SwipeItem } from "vant";
 import { Tabbar } from "@/components";
 export default {
   name: "Home",
   components: {
-    // [Swipe.name]: Swipe,
-    // [SwipeItem.name]: SwipeItem,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
     Tabbar
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
